109659e0f08705d97a9d4f0f5bb0dc23405ccde3
Ustun Ozgur <ustunozgur@gmail.com>
2012-03-12 15:00:01 +0200

    Remove elpa from git

--- elpa/bubbles-0.5/bubbles-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; bubbles-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (bubbles) "bubbles" "bubbles.el" (19709 29360))
-;;; Generated autoloads from bubbles.el
-
-(autoload 'bubbles "bubbles" "\
-Play Bubbles game.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("bubbles-pkg.el") (19709 29360 358147))
-
-;;;***
-
-(provide 'bubbles-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; bubbles-autoloads.el ends here
